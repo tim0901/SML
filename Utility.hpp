d0e26d77ef6cb963249446199e6985f320ba662a
Alex <alexander1.richardson@gmail.com>
2025-07-16 13:55:49 +0100

    Angle conversion utility functions

--- Utility.hpp
+++ Utility.hpp
@@ -6,5 +6,18 @@ namespace sml {
 	sml_export template<typename T>
 	concept arithmetic = std::integral<T> or std::floating_point<T>;
 
+	constexpr double DegToRad = std::numbers::pi / 180.0;
+	constexpr double RadToDeg = std::numbers::inv_pi * 180.0;
+
+	sml_export template<arithmetic T>
+	double DegreesToRadians(const T& theta) {
+		return (theta * DegToRad);
+	}
+
+	sml_export template<arithmetic T>
+		double RadiansToDegrees(const T& rads) {
+		return (rads * RadToDeg); 
+	}
+
 }
 #endif // SML_UTILITY_HPP
